d2a885c6658c0ec161aba9bc3a62535c2eb403e1
LeoKucing <marifakbarvix@gmail.com>
2025-07-09 11:43:08 +0700

    3

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const cors = require('cors');
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 
-server.use(cors()); // kalau mau bisa diakses dari frontend
-server.use(middlewares);
-server.use(router);
+server.use(middlewares)
+server.use(jsonServer.bodyParser)
+server.use('/api', router)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 server.listen(port, () => {
-    console.log(`JSON Server running on port ${port}`);
-});
+    console.log(`JSON Server is running on port ${port}`)
+})
